921d88cdbab1bfa597227424d314d6894ddbe1c1
Nidhi Patel <patelnidhi172001@gmail.com>
2026-06-11 22:12:08 +0530

    initial commit

---
+++ src/components/home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+<div className="p-4 text-center">
+  <h1>Home Page</h1>
+</div>
+  )
+};
+
+export default Home;
---
+++ src/components/user.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+function User() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error("Error fetching users:", error));
+  }, []);
+
+
+  return (
+    <div className="p-4 text-center">
+      <h1>User List</h1>
+      <ul>
+        {users?.length > 0 && users.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default User;
